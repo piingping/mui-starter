d78a4d88072c4eb639568b372e75bf680223480e
piingping <sasiyakorn.ping@gmail.com>
2025-07-04 18:10:26 +0700

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { CircularProgress, Box, Typography} from "@mui/material";
 import JobList from "./pages/JobList/JobList.tsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import JobDetails from "./pages/JobDetails/JobDetails";
-
+import ReviewRegisteration from "./pages/ReviewRegisteration/ReviewRegisteration";
+import SubmittedPage from "./pages/SubmittedPage/SubmittedPage.tsx";
 function App() {
   const { isReady, isLoggedIn, profile, error } = useLiff();
 
@@ -17,7 +18,10 @@ function App() {
         {isLoggedIn && profile ? (
           <Routes>
             <Route path="/" element={<JobList />} />
+            <Route path="/jobs/:id/reviewRegister" element={<ReviewRegisteration />} />
             <Route path="/jobs/:id" element={<JobDetails />} />
+            <Route path="/jobs/:id/submitted" element={<SubmittedPage />} />
+          
           </Routes>
         ) : (
           <Typography variant="body1">Please login via LINE</Typography>
